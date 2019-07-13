6052d8e6f9c65fe056dfcf731dfe6d9829d302df
joshuadconrad <joshuadconrad@gmail.com>
2019-07-13 11:44:43 -0500

    Checkpoint-2967 Submission

---
+++ main.js
@@ -0,0 +1,18 @@
+const greetings = require("./greeting.js");
+
+const responses = require("./response.js");
+
+
+greetings.greetingEnglish();
+responses.responseEnglish();
+
+
+greetings.greetingJapanese();
+responses.responseJapanese();
+
+
+greetings.greetingSpanish();
+responses.responseSpanish();
+
+greetings.greetingIcelandic();
+responses.responseIcelandic();
---
+++ response.js
@@ -0,0 +1,15 @@
+exports.responseEnglish = function(){
+ console.log("Hello, how are you?");
+}
+
+exports.responseJapanese = function(){
+ console.log("Konichiwa, genkidesuka?");
+}
+
+exports.responseSpanish = function(){
+ console.log("¿Hola como estas?");
+}
+
+exports.responseIcelandic = function(){
+ console.log("halló, hvernig ertu?");
+}
